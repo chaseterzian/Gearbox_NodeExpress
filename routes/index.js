de4c40eb14f3f1a4d44ce366ef0e59db8941a1c4
ChaseTerzian <chaseterzian@gmail.com>
2016-01-19 13:51:59 -0700

    langin page added, links to empty testing area, working

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('landing.html.ejs');
+});
+/* GET testing zone (index) page. */
+router.get('/testing-area', function(req, res, next) {
   res.render('index.html.ejs');
 });
 
